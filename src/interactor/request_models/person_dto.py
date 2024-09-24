17e3073986b491f26500fe7a7662580d893a28ba
Gerardo Rios <jose.g.rios@unl.edu.ec>
2024-09-24 18:03:45 -0500

    Added person request models

---
+++ src/interactor/request_models/person_dto.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
+class CreatePersonRequest:
+    name: str
+    phone: str
+    address: str
+    
+
+
+@dataclass
+class GetPersonByPhoneRequest:
+    phone: str
---
+++ src/interactor/response_models/person_output_dto.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+@dataclass
+class CreatePersonResponse:
+    person_id: str
+
+@dataclass
+class GetPersonResponse:
+    person_id: str
+    name: str
+    phone: str
+    address: str
+    city: str
+    country: str
