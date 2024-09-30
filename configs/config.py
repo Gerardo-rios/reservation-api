e91000f808a16899d1a9a261def2874e63c72120
Gerardo Rios <jose.g.rios@unl.edu.ec>
2024-09-30 16:12:30 -0500

    Updated docker compose

--- configs/config.py
+++ configs/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv('MYSQL_HOST', 'mysql-service')
+DB_HOST = os.getenv('DB_HOST', 'mysql-service')
 DB_PORT = os.getenv("DB_PORT", 3306)
 DB_NAME = os.getenv("DB_NAME")
 DB_DRIVER = "mysql+pymysql"
